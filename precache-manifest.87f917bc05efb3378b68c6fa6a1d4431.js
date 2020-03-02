4f589f714f981821e87ac973329494b5d5da9dac
Long Vo <gaming2000hcm@gmail.com>
2020-03-02 09:25:51 +0200

    Updates

---
+++ precache-manifest.87f917bc05efb3378b68c6fa6a1d4431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98a6d14f5b1facb7fdc072e5c1b997a",
+    "url": "/exam_platform_client/index.html"
+  },
+  {
+    "revision": "f44813ef142b770edb5b",
+    "url": "/exam_platform_client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6839b74d504013aebd77",
+    "url": "/exam_platform_client/static/js/2.75bcaec0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/exam_platform_client/static/js/2.75bcaec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44813ef142b770edb5b",
+    "url": "/exam_platform_client/static/js/main.f8ed3c50.chunk.js"
+  },
+  {
+    "revision": "f68b565f57917e0f7967",
+    "url": "/exam_platform_client/static/js/runtime-main.384344cd.js"
+  }
+]);
